93b569088171c90235ff9891bd470af677ae501b
正在收敛但不确定 <realmenghantian@gmail.com>
2026-05-17 22:41:19 -0500

    Update embedding.py

--- cs336_basics/embedding.py
+++ cs336_basics/embedding.py
@@ -10,11 +10,11 @@ class Embedding(nn.Module):
         dtype: torch.dtype | None = None  Data type of the parameters
         """
         super().__init__()
-        embedding_mat = torch.empty((num_embeddings, embedding_dim), device=device, dtype=dtype)
+        embedding_mat = torch.empty((num_embeddings, embedding_dim), device=device, dtype=dtype)#(vocab_size, d_model)
         torch.nn.init.trunc_normal_(embedding_mat, mean=0.0, std=1, a=-3, b=3)
-        self.embedding_mat = nn.Parameter(embedding_mat)
+        self.embedding_mat = nn.Parameter(embedding_mat) 
 
-    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+    def forward(self, token_ids: torch.LongTensor) -> torch.Tensor:#token_ids形状 (batch_size, sequence_length)，是一个 LongTensor，表示输入的 token ID 序列。
         """
         Lookup the embedding vectors for the given token IDs.
         """
